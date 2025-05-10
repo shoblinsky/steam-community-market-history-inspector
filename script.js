d99afc76d95bb0755c308bccf37959ec15a1608c
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-10 23:49:10 +0500

    Works fine in my active listings.
    Don't work yet in My Market History. My bad.

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 function handleHover() {
     const hoverMenu = document.querySelector("#iteminfo_clienthover");
     if (!hoverMenu) {
-        console.log("There is no #iteminfo_clienthover element in DOM yet. Return. ");
+        console.log("There is no #iteminfo_clienthover element in DOM yet. Probably, your window's width less than 1000px. Return. ");
         return;
     }
 
     const inspectLink = hoverMenu.querySelector("a.btn_small.btn_grey_white_innerfade[href^='steam://']");
     if (!inspectLink) {
-        console.log("There is no 'Inspect in game' button element yet. Return. ");
+        console.log("There is no 'Inspect in game' button element yet. Probably, your window's width less than 1000px. Return. ");
         return;
     }
 
-    console.log(`Here is the inspect link: ${inspectLink.href}`);
-}
+    const itemName = hoverMenu.querySelector("#hover_item_name");
+    console.log(`Here is the inspect link of "${itemName.textContent}": ${inspectLink.href}`);
+};
+
+function handleHoverListeners() {
+    const rows = document.querySelectorAll(".market_listing_row.market_recent_listing_row");
+
+    rows.forEach(row => {
+        const link = row.querySelector("span.market_listing_item_name_link[href*='/730/']");
+        if (!link) return;
+
+        const image = row.querySelector(".market_listing_item_img.economy_item_hoverable");
+        const name = row.querySelector(".market_listing_item_name.economy_item_hoverable");
+
+        if (image && name) {
+            image.addEventListener("mouseenter", handleHover);
+            name.addEventListener("mouseenter", handleHover);
+        }
+    });
+};
+
+handleHoverListeners()
+
