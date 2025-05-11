1c6b1dd7822d50b6cca1334ec680c6eca846619f
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-11 11:27:00 +0500

    trying to fix "my market history"

--- script.js
+++ script.js
@@ -16,10 +16,12 @@ function handleHover() {
 };
 
 function handleHoverListeners() {
-    const rows = document.querySelectorAll(".market_listing_row.market_recent_listing_row");
+    const rows = (document.querySelectorAll(".market_listing_row.market_recent_listing_row"));
 
     rows.forEach(row => {
-        const link = row.querySelector("span.market_listing_item_name_link[href*='/730/']");
+        const link = row.querySelector("a.market_listing_item_name_link[href*='/730/']")
+        // ("span.market_listing_item_name_link[href*='/730/']");
+        // ("div.item_actions.economy_item_popup_only > a.btn_small.btn_grey_white_innerfade[href*='/730/']")
         if (!link) return;
 
         const image = row.querySelector(".market_listing_item_img.economy_item_hoverable");
