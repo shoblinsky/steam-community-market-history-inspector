fbb225260e1840e1bcdb6c77e1ee97fee3d0dc95
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-11 12:04:55 +0500

    Done for history, don't work with other pages yet

--- script.js
+++ script.js
@@ -19,10 +19,18 @@ function handleHoverListeners() {
     const rows = (document.querySelectorAll(".market_listing_row.market_recent_listing_row"));
 
     rows.forEach(row => {
+        const gameName = 'Counter-Strike 2';
+        /*
+        Code of 'active listings' handler.
         const link = row.querySelector("a.market_listing_item_name_link[href*='/730/']")
-        // ("span.market_listing_item_name_link[href*='/730/']");
-        // ("div.item_actions.economy_item_popup_only > a.btn_small.btn_grey_white_innerfade[href*='/730/']")
+        ("span.market_listing_item_name_link[href*='/730/']");
+        ("div.item_actions.economy_item_popup_only > a.btn_small.btn_grey_white_innerfade[href*='/730/']")
         if (!link) return;
+        */
+        const game = row.querySelector("span.market_listing_game_name")
+        if (game.textContent !== gameName) {
+            return;
+        }
 
         const image = row.querySelector(".market_listing_item_img.economy_item_hoverable");
         const name = row.querySelector(".market_listing_item_name.economy_item_hoverable");
